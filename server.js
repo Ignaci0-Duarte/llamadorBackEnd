43303eff7b5530eb42daf291e97712e4e57b5fda
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-04-03 16:22:39 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,77 @@
+/* const WebSocket = require("ws");
+
+// Crear un nuevo servidor WebSocket
+const wss = new WebSocket.Server({ host: "0.0.0.0", port: 3000 });
+
+// Mantener una referencia a los clientes conectados
+const clients = new Set();
+
+// Manejar la conexión de un cliente WebSocket
+wss.on("connection", function connection(ws) {
+  console.log("Nuevo cliente conectado");
+
+  // Agregar el cliente a la lista de clientes conectados
+  clients.add(ws);
+
+  // Manejar mensajes entrantes
+  ws.on("message", function incoming(message) {
+    console.log("Mensaje recibido:", message);
+
+    // Enviar el mensaje a todos los clientes conectados excepto al remitente
+    clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Manejar cierre de conexión
+  ws.on("close", function () {
+    console.log("Cliente desconectado");
+    // Remover el cliente de la lista de clientes conectados
+    clients.delete(ws);
+  });
+});
+ */
+
+const WebSocket = require("ws");
+
+// Crear un nuevo servidor WebSocket
+const wss = new WebSocket.Server({ host: "0.0.0.0", port: 3000 });
+
+// Mantener una referencia a los clientes conectados
+const clients = new Set();
+
+// Manejar la conexión de un cliente WebSocket
+wss.on("connection", function connection(ws) {
+  console.log("Nuevo cliente conectado");
+
+  // Agregar el cliente a la lista de clientes conectados
+  clients.add(ws);
+
+  // Manejar mensajes entrantes
+  ws.on("message", function incoming(message) {
+    console.log("Mensaje recibido:", message);
+
+    // Enviar el mensaje a todos los clientes conectados excepto al remitente
+    clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Manejar cierre de conexión
+  ws.on("close", function () {
+    console.log("Cliente desconectado");
+    // Remover el cliente de la lista de clientes conectados
+    clients.delete(ws);
+  });
+});
+
+// Mensaje de conexión
+wss.on("listening", function () {
+  console.log(
+    `Servidor WebSocket corriendo en ws://${wss.options.host}:${wss.options.port}`
+  );
+});
